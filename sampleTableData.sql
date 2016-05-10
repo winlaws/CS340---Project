d0674f0ea7637edb975ae7568851055128b34e2b
Spencer Winlaw <winlaws@oregonstate.edu>
2016-05-10 12:24:49 -0400

    sample data structure

---
+++ sampleTableData.sql
@@ -0,0 +1,18 @@
+INSERT INTO location (streetAddress, city, state, zip) 
+VALUES ();
+
+INSERT INTO user (username, password, lid) 
+VALUES ();
+
+INSERT INTO tag (description) 
+VALUES ();
+
+INSERT INTO restaurant (name, website, phone, lid) 
+VALUES ();
+
+INSERT INTO tag_restaurant (rid, tid, phone, lid) 
+VALUES ();
+
+INSERT INTO review (uid, rid, rating, reviewtxt) 
+VALUES ();
+
